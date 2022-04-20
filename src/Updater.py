2b87490986d4cfdb872eb3cb7d596c654446cb41
qhy040404 <qhy040404@163.com>
2022-04-20 10:12:43 +0800

    Add CI and update

--- src/Updater.py
+++ src/Updater.py
@@ -22,8 +22,9 @@ s.headers = {
 }
 
 # initialize consts
-github_release_api_url1 = 'https://api.github.com/repos/{owner}/{repo}/releases'
-#github_release_api_url2 = 'https://api.github.com/repos/{owner}/{repo}/releases'
+github_release_api_url1 = 'https://api.github.com/repos/qhy040404/DLUT-library-auto-reservation/releases'
+github_release_api_url2 = 'https://api.github.com/repos/qhy040404/Library-reservation-configGenerator/releases'
+github_release_api_url3 = 'https://api.github.com/repos/qhy040404/Library-reservation-updater/releases'
 
 # read config.json
 with open("config.json","r") as conf:
@@ -32,29 +33,51 @@ with open("config.json","r") as conf:
 
 if updateConf.get('channel') == 'stable':
     github_release_api_url1 += '/latest'
+    github_release_api_url2 += '/latest'
+    github_release_api_url3 += '/latest'
 elif updateConf.get('channel') == 'beta':
     github_release_api_url1 += '?per_page=1'
+    github_release_api_url2 += '?per_page=1'
+    github_release_api_url3 += '?per_page=1'
 else:
     print('Unknown channel.')
 
-{module1} = updateConf.get({module1})
-## add more modules
+mainver = updateConf.get('main')
+configGeneratorver = updateConf.get('configGenerator')
+Updaterver = updateConf.get('Updater')
 
 response1 = s.get(github_release_api_url1, verify = False).text.strip('[]')
 remoteVer1 = json.loads(response1).get('tag_name')
 
+response2 = s.get(github_release_api_url2, verify = False).text.strip('[]')
+remoteVer2 = json.loads(response2).get('tag_name')
+
+response3 = s.get(github_release_api_url3, verify = False).text.strip('[]')
+remoteVer3 = json.loads(response3).get('tag_name')
+
 # compare local and remote
-if remoteVer1 is {module1}:
+if remoteVer1 == mainver and remoteVer2 == configGeneratorver and remoteVer3 == Updaterver:
     print('You have the latest release.')
 else:
-    print('A newer version has been released.')
-    print('New:' + remoteVer1)
-    print(json.loads(response1).get('body'))
+    print('One or more modules\' newer version has been released.')
+    print()
+    if remoteVer1 is not mainver:
+        print('New: main: ' + remoteVer1)
+        print(json.loads(response1).get('body'))
+        print()
+    if remoteVer2 is not configGeneratorver:
+        print('New: configGenerator: ' + remoteVer2)
+        print(json.loads(response2).get('body'))
+        print()
+    if remoteVer3 is not Updaterver:
+        print('New: Updater: ' + remoteVer3)
+        print(json.loads(response3).get('body'))
+        print()
 
     updateChoice = input('Update?(Y/N)')
     if updateChoice == 'Y' or updateChoice == 'y':
         os.mkdir('update')
-        updateConf.update({module1} = remoteVer1)
+        updateConf.update(main = remoteVer1, configGenerator = remoteVer2, Updater = remoteVer3)
         with open("update/config.json","w") as conf:
             json.dump(updateConf, conf)
     else:
@@ -69,14 +92,35 @@ else:
     else:
         print('Unknown system')
 
-    download_url = 'https://github.com/{owner}/{repo}/releases/download/' + remoteVer1 + '/{package_name}-' + sysType + '.zip'
-    path = 'temp.zip'
-    try:
-        wget.download(download_url, path)
-    except Exception as e:
-        print('Error')
-        print(e)
-        sys.exit()
+    if remoteVer1 is not mainver:
+        download_url = 'https://github.com/qhy040404/DLUT-library-auto-reservation/releases/download/' + remoteVer1 + '/Library-' + sysType + '.zip'
+        path = 'temp.zip'
+        try:
+            wget.download(download_url, path)
+        except Exception as e:
+            print('Error')
+            print(e)
+            sys.exit()
+    else:
+        if remoteVer2 is not configGeneratorver:
+            download_url = 'https://github.com/qhy040404/Library-reservation-configGenerator/releases/download/' + remoteVer1 + '/ConfigGenerator-' + sysType + '.zip'
+            path = 'temp.zip'
+            try:
+                wget.download(download_url, path)
+            except Exception as e:
+                print('Error')
+                print(e)
+                sys.exit()
+        if remoteVer3 is not Updaterver:
+            download_url = 'https://github.com/qhy040404/Library-reservation-updater/releases/download/' + remoteVer1 + '/Updater-' + sysType + '.zip'
+            path = 'temp.zip'
+            try:
+                wget.download(download_url, path)
+            except Exception as e:
+                print('Error')
+                print(e)
+                sys.exit()
+
 
     un_zip('temp.zip')
 
