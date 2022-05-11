b177475301008e620a2155336138551818345d65
qhy040404 <qhy040404@163.com>
2022-05-11 20:24:05 +0800

    Revert to PyCharm and reformat imports

--- src/Updater.py
+++ src/Updater.py
@@ -1,14 +1,16 @@
 # import
+import json
 import os
-import sys
 import platform
-import time
 import shutil
+import sys
+import time
 import zipfile
+
 import requests
 import urllib3
-import json
 import wget
+
 
 # define func
 def un_zip(file_name):
